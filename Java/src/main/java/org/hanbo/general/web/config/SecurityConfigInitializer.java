ed51feca0b47a05e944837eb584702cf90182bfc
Deventankse <117820352+Deventankse@users.noreply.github.com>
2023-06-09 13:13:09 +0530

    Delete Java/src/main directory

--- Java/src/main/java/org/hanbo/general/web/config/SecurityConfigInitializer.java
+++
@@ -1,8 +0,0 @@
-package org.hanbo.general.web.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityConfigInitializer
-   extends AbstractSecurityWebApplicationInitializer
-{
-}
--- Java/src/main/java/org/hanbo/general/web/security/LoginUserService.java
+++
@@ -1,8 +0,0 @@
-package org.hanbo.general.web.security;
-
-import org.hanbo.general.web.security.models.UserModel;
-
-public interface LoginUserService
-{
-   UserModel getLoginUser(String userName);
-}
